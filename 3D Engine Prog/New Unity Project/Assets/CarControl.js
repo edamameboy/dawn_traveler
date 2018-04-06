527950e60b571408d00ebd0b030a6577187addb6
Allumarge <37730381+Allumarge@users.noreply.github.com>
2018-04-07 00:52:20 +0700

    File Game

---
+++ 3D Engine Prog/New Unity Project/Assets/CarControl.js
@@ -0,0 +1,12 @@
+﻿
+function Update () {
+	if(Input.GetKey(KeyCode.UpArrow))
+		transform.Translate(Vector3(0,0,1));
+	if(Input.GetKey(KeyCode.DownArrow))
+		transform.Translate(Vector3(0,0,-1));
+	if(Input.GetKey(KeyCode.RightArrow))
+		transform.Rotate(Vector3(0,1,0));
+	if(Input.GetKey(KeyCode.LeftArrow))
+		transform.Rotate(Vector3(0,-1,0));
+	
+}
---
+++ 3D Engine Prog/New Unity Project/Assets/Roda.js
@@ -0,0 +1,7 @@
+﻿
+function Update () {
+	if(Input.GetKey(KeyCode.UpArrow))
+		transform.Rotate(Vector3(10,0,0));
+	if(Input.GetKey(KeyCode.DownArrow))
+		transform.Rotate(Vector3(-10,0,0));
+}
